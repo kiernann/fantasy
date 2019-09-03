a33d9617e06162974203bf9159c210342e80dba7
Kiernan Nicholls <kiernann@protonmail.com>
2019-09-02 22:10:09 -0400

    Plot win history per season

--- code/api_matchup.R
+++ code/api_matchup.R
@@ -2,12 +2,15 @@ library(tidyverse)
 library(httr)
 library(glue)
 
+# query matchup api --------------------------------------------------------------------------
+
 league_id <- 252353
 url <- glue("https://fantasy.espn.com/apis/v3/games/ffl/leagueHistory/{league_id}")
 
 response <- GET(url, query = list(view = "mMatchup"))
 content <- content(response)
 
+# create function to tibble matchup
 format_matchup <- function(z) {
   tibble(
     game = z$id,
@@ -18,6 +21,7 @@ format_matchup <- function(z) {
   )
 }
 
+# extract and format every matchup
 matchup_history <- rep(list(NA), length(content))
 for (i in seq_along(content)) {
   matchups <- map(content[[i]]$schedule, format_matchup)
@@ -25,6 +29,8 @@ for (i in seq_along(content)) {
     mutate(year = content[[i]]$seasonId) %>% 
     select(year, game, week, home, team, score)
 }
+
+# query team history -------------------------------------------------------------------------
 
 response <- GET(url, query = list(view = "mTeam"))
 content <- content(response)
@@ -39,6 +45,7 @@ for (i in seq_along(content)) {
 
 team_history <- bind_rows(team_history)
 
+# create best abbrevs by hand
 team_history <- tribble(
   ~team, ~abbrev,
   1,  "AGUS",
@@ -54,16 +61,38 @@ team_history <- tribble(
   11, "KYLE",
 )
 
-bind_rows(matchup_history) %>% 
+# join abbrevs and calc wins
+matchup_history <- 
+  bind_rows(matchup_history) %>% 
   left_join(team_history, by = "team") %>% 
-  select(
-    year,
-    game,
-    week,
-    home,
-    team,
-    abbrev,
-    score
-  ) %>% 
   group_by(year, game, week) %>% 
-  mutate(win = score == max(score))
+  mutate(win = score == max(score)) %>% 
+  ungroup()
+
+# save matchup history
+write_csv(matchup_history, "data/matchup_history.csv")
+
+# plot win history
+wins_history <- 
+  matchup_history %>%
+  arrange(year, game) %>% 
+  group_by(year, team) %>% 
+  mutate(wins = cumsum(win)) %>% 
+  ggplot(aes(x = game, y = wins)) +
+  geom_line(aes(color = abbrev), size = 2) + 
+  facet_wrap(~year) +
+  labs(
+    title = "Wins Over Time",
+    x = "Game of Season",
+    y = "Total Wins",
+    color = "Team"
+  )
+
+# save matchup history plot
+ggsave(
+  filename = "plots/wins_history.png",
+  plot = wins_history,
+  dpi = "retina",
+  width = 9,
+  height = 5
+)
