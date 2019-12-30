c631ac281aa51032220482ac763094541ce2b788
Kiernan Nicholls <kiernann@protonmail.com>
2019-12-29 19:35:28 -0500

    Exclude playoffs in plots

--- code/plot_history.R
+++ code/plot_history.R
@@ -1,4 +1,5 @@
 matchups %>% 
+  filter(as.integer(week) < 13) %>% 
   group_by(id) %>% 
   summarise(wins = mean(winner, na.rm = )) %>% 
   ggplot(aes(x = reorder(id, wins), y = wins)) + 
@@ -20,15 +21,24 @@ ggsave(
 )
 
 matchups %>% 
+  filter(as.integer(week) < 13) %>% 
   group_by(id) %>% 
   summarise(mean = mean(score, na.rm = )) %>% 
   ggplot(aes(x = reorder(id, mean), y = mean)) + 
   geom_col(aes(fill = id)) + scale_fill_fantasy() +
   theme(legend.position = "none") + 
-  coord_cartesian(ylim = c(80, 120)) +
+  coord_cartesian(ylim = c(80, 110)) +
   labs(
     title = "Mean Score by Team",
-    subtitle = "Scoring settings and number of teams changing",
+    subtitle = "Scoring and number of teams changing year to year",
     x = "Team", 
     y = "Rate"
   )
+
+ggsave(
+  filename = "plots/avg-score.png",
+  plot = last_plot(),
+  dpi = "retina",
+  height = 5,
+  width = 9
+)
